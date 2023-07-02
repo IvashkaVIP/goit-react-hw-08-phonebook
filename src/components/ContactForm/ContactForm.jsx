520632708357772b9983ae92d5936eb18b1ca58c
IvashkaVIP <ivashka.co@gmail.com>
2023-07-02 17:07:23 +0300

    add pattern Name

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -27,29 +27,30 @@ export class ContactForm extends Component {
   render() {
     return (
       <>
-        
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name
-              <input
-                type="text"
-                name="name"
-                value={this.state.name}
-                onChange={this.handleChange}
-              ></input>
-            </label>
-            <label>
-              Number
-              <input
-                type="text"
-                name="number"
-                value={this.state.number}
-                onChange={this.handleChange}
-              ></input>
-            </label>
-            <button type="submit">Add contact</button>
-          </form>
-        
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            ></input>
+          </label>
+          <label>
+            Number
+            <input
+              type="text"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleChange}
+            ></input>
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
       </>
     );
   }

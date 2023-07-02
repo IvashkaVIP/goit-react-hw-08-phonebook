35fd403b1297dfca04243ca8569c7965ea596060
IvashkaVIP <ivashka.co@gmail.com>
2023-07-02 18:26:21 +0300

    add Filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { nanoid } from 'nanoid';
 import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
@@ -11,27 +12,44 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+    filter: '',
     name: '',
     number: '',
   };
 
-  formSubmitHandler = ({name, number}) => {
-    const contact = { number, name, id: nanoid() };
-    this.setState(prevState => ({
-      contacts: [contact, ...prevState.contacts],
+  changeFilter = evt => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
 
+  formSubmitHandler = ({ name, number }) => {
+    const contact = { number, name, id: nanoid() };
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
     }));
   };
 
+  getVisibleContacts = ({filter, contacts}) => {
+    
+    const normalizeFilter = filter.toLowerCase();
+    return contacts.filter(item => 
+    item.name.toLowerCase().includes(normalizeFilter),
+    );
+    
+  }
+
   render() {
-    const { contacts } = this.state;
+    const { contacts, filter } = this.state;
+    const visibleContacts = this.getVisibleContacts(this.state);
     
     return (
       <div>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
         <h1>Contacts </h1>
-        <ContactList contacts={contacts} />
+
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <ContactList contacts={visibleContacts} />
 
         {/* <h2>Contacts</h2>
   <Filter ... />

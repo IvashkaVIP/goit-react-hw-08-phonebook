d77e5bbe6de9a96112d1a47212f6fbd9a16493d7
IvashkaVIP <ivashka.co@gmail.com>
2023-07-02 22:37:56 +0300

    add css

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -15,6 +15,7 @@ export const ContactList = ({ contacts, onDeleteContact }) => (
 );
 
 ContactList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired,
 };
+

55effd9a23158ed142aa920439c70628995c8b69
IvashkaVIP <ivashka.co@gmail.com>
2023-07-15 17:51:15 +0300

    add Hooks on ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,53 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { nanoid } from 'nanoid';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import css from './App.module.css';
 
-export class App extends Component {
+export const App = () => {
+
+const [contacts, setContacts] = useState(
+  [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }]
+  );
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = ({ name, number }) => {
+     if (contacts.find(item => item.name === name)) {
+       alert(`${name} is already in contacts.`);
+       return;
+     }
+     setContacts(prev => [{ number, name, id: nanoid() }, ...prev]);
+   };
+  console.log(contacts);
+
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} />
+      <h1>Contacts </h1>
+      {/* <Filter value={filter} onChange={this.changeFilter} />
+      <ContactList
+        contacts={visibleContacts}
+        onDeleteContact={this.deleteContact}
+      /> */}
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------
+export class oldApp extends Component {
   state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,82 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react';
+import { useState } from 'react';
 import css from './ContactForm.module.css';
 
-export class ContactForm extends Component {
+
+
+
+export const ContactForm = ({ onSubmit }) => {
+  
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('');
+
+    const handleSubmit = evt => {
+      evt.preventDefault();
+      onSubmit({name, number});
+      reset();
+  };
+
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:  return;
+    }
+    
+  };
+  
+    const reset = () => {
+      setName('');
+      setNumber('');
+    };
+
+      return (
+      <div>
+        <form onSubmit={handleSubmit} className={css.form}>
+          <label className={css.label}>
+            Name
+            <input
+              className={css.input}
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            ></input>
+          </label>
+          <label className={css.label}>
+            Number
+            <input
+              className={css.input}
+              type="tel"
+              name="number"
+              value={number}
+              onChange={handleChange}
+              pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            ></input>
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
+    );
+  
+  
+}
+
+
+//----------------------------------------------------------------
+
+export class oldContactForm extends Component {
   state = {
     name: '',
     number: '',

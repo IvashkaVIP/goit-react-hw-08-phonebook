b1f2c14811d724bb36b0bbb3932296c16ebab6dd
IvashkaVIP <ivashka.co@gmail.com>
2023-08-05 13:34:19 +0300

    add testComp createSlice

--- src/components/TestComponent/TestComponent.jsx
+++ src/components/TestComponent/TestComponent.jsx
@@ -1,10 +1,18 @@
-import{useSelector, useDispatch} from 'react-redux'
+// src > components > TestComp > TestComp.jsx
+
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from 'redux/store';
 
 
 export const TestComponent = () => {
+
+    const dispatch = useDispatch();
+    const value = useSelector(state=>state.myValue)
     return (
-        <>
-        <h1>Test : </h1>
-        </>
-    )
+      <>
+        <h1>Test : {value}</h1>
+        <button onClick={() => dispatch(increment(5))}>Increment</button>
+        <button onClick={() => dispatch(decrement(5))}>Decrement</button>
+      </>
+    );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,43 @@
+// src > redux > store.js
+
 import { configureStore } from '@reduxjs/toolkit';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+// import { createAction, createReducer} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-const increment = createAction('myValue/increment');
-const decrement = createAction('myValue/decrement');
+// export const increment = createAction('myValue/increment');
+// export const decrement = createAction('myValue/decrement');
 
-const myReducer = createReducer(55, {
-  [increment]: (state, action) => state + action.payload,
-  [decrement]: (state, action) => state - action.payload,
-});
+// const myReducer = createReducer(55, {
+//   [increment]: (state, action) => state + action.payload,
+//   [decrement]: (state, action) => state - action.payload,
+// });
+
+// export const store = configureStore({
+//     reducer: {
+//         myValue: myReducer,
+//   },
+// });
+
+// --------------------------------  Использование creatSlice
+
+const myValueSlice = createSlice({
+    name: 'myValue',
+    initialState: 150,
+    reducers: {
+        increment(state, action) {
+            return state + action.payload;
+        },
+        decrement(state, action) {
+            return state - action.payload;
+            },
+    },
+})
 
 export const store = configureStore({
-    reducer: {
-        myValue: myReducer,
+  reducer: {
+    myValue: myValueSlice.reducer,
   },
 });
 
+export const { increment , decrement } = myValueSlice.actions;
 

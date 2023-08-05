db11982c1e1b674b2c33cee1d1bfe7b5d6f4a74b
IvashkaVIP <ivashka.co@gmail.com>
2023-08-05 13:36:27 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,7 @@
 // src > redux > store.js
 
 import { configureStore } from '@reduxjs/toolkit';
-// import { createAction, createReducer} from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
-
-// export const increment = createAction('myValue/increment');
-// export const decrement = createAction('myValue/decrement');
-
-// const myReducer = createReducer(55, {
-//   [increment]: (state, action) => state + action.payload,
-//   [decrement]: (state, action) => state - action.payload,
-// });
-
-// export const store = configureStore({
-//     reducer: {
-//         myValue: myReducer,
-//   },
-// });
 
 // --------------------------------  Использование creatSlice
 
@@ -40,4 +25,24 @@ export const store = configureStore({
 });
 
 export const { increment , decrement } = myValueSlice.actions;
+
+
+//------------------------------------ без creatSlice
+
+// import { createAction, createReducer} from '@reduxjs/toolkit';
+
+
+// export const increment = createAction('myValue/increment');
+// export const decrement = createAction('myValue/decrement');
+
+// const myReducer = createReducer(55, {
+//   [increment]: (state, action) => state + action.payload,
+//   [decrement]: (state, action) => state - action.payload,
+// });
+
+// export const store = configureStore({
+//     reducer: {
+//         myValue: myReducer,
+//   },
+// });
 

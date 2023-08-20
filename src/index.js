f603f6909e69fa55b27729686b962d7eefa0ff4f
IvashkaVIP <ivashka.co@gmail.com>
2023-08-20 20:43:04 +0300

    add persist

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom'
-import { store } from './redux/store';
+import { store, persistor } from 'redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+// import { store } from './app/store';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,41 @@ import { filterSlice } from './filter/filterSlice';
 import { contactsSlice } from './contacts/contactsSlice';
 import { authSlice } from './auth/authSlice'
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+ 
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedAuth = persistReducer(persistConfig, authSlice.reducer);
+
 const rootReducer = combineReducers({
   allContacts: contactsSlice.reducer,
   filter: filterSlice.reducer,
-  auth: authSlice.reducer,
+  auth: persistedAuth,
 });
+
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);

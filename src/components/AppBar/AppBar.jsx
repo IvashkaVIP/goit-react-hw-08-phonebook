4c7212d7730179def163af453994aedbceda962a
IvashkaVIP <ivashka.co@gmail.com>
2023-08-22 22:54:10 +0300

    up

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,19 +1,3 @@
-// import { Navigation } from '../Navigation/Navigation';
-import { AuthNav } from 'components/AuthNav/authNav';
-import { UserMenu } from 'components/UserMenu/userMenu';
-import css from '../App.module.css'
-import { useSelector } from 'react-redux';
-import { authSelectors } from 'redux/auth/authSelectors';
-// import { Contacts } from 'pages/contacts';
-
-
 export const AppBar = () => {
-  const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn)
-  
-    return (
-      <header className={css.header}>
-        {/* <Navigation /> */}
-        {isLoggedIn ? <UserMenu/> : <AuthNav/>}
-      </header>
-    );
+    
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
-// import { Toaster } from 'react-hot-toast';
-// import { AppBar } from '../AppBar/AppBar';
+import { AppBar } from 'components/AppBar/AppBar';
 import { Suspense } from 'react';
 import css from './Layout.module.css'
 
 export const Layout = () => {
   return (
     <div className={css.container}>
-      {/* <AppBar /> */}
+      <AppBar />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>

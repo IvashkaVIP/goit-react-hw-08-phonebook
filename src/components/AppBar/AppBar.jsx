3faa606419eaac0fb7f2089b3e19b7e4ea848e8a
IvashkaVIP <ivashka.co@gmail.com>
2023-08-22 23:01:59 +0300

    up App

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,3 +1,19 @@
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { authSelectors } from "redux/auth/authSelectors";
+import { UserMenu } from "components/UserMenu/userMenu";
+import { AuthNav } from "components/AuthNav/authNav";
+import css from './AppBar.module.css'
+
+
 export const AppBar = () => {
-    
+     const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
+
+     return (
+       <header className={css.header}>
+         {/* <Navigation /> */}
+         {isLoggedIn ? <UserMenu /> : <AuthNav />}
+       </header>
+     );
+
+
 }

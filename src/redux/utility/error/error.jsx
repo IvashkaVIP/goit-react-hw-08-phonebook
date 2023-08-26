809f8de8a5f5a6612c340a546cc6d3805d61b169
IvashkaVIP <ivashka.co@gmail.com>
2023-08-26 10:57:39 +0300

    Update error.jsx

--- src/redux/utility/error/error.jsx
+++ src/redux/utility/error/error.jsx
@@ -1,20 +1,18 @@
 import { utilitySelectors } from '../utilitySelectors';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import { resetError } from 'redux/utility/utilitySlice';
 import css from './error.module.css';
 
 export const Error = () => {
   const isError = useSelector(utilitySelectors.selectIsError);
-  const currentLocation = useLocation();
   const dispatch = useDispatch();
  
-  useEffect(() => {
-   isError && dispatch(resetError());
- }, [dispatch, currentLocation.key]);
-  
-
+   if (isError) {
+     setTimeout(() => {
+       dispatch(resetError())
+     }, 3000);
+   }
+   
   return (
     isError && (
       <>
